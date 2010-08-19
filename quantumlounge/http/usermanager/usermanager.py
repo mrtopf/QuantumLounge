c7b282854cf1e27195305f9a63611b1235c606f0
Christian Scholz <cs@comlounge.net>
2010-08-19 22:58:22 +0200

    removed unused files

--- quantumlounge/http/usermanager/usermanager.py
+++
@@ -1,9 +0,0 @@
-from quantumlounge.framework import Handler, Application
-import api
-
-class UserManager(Application):
-    """a RESTful user manager"""
-    
-    handlers = [
-        ('/api/login', api.LoginHandler)
-    ]
