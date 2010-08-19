cde9e3f1248e7e0f650b7b25c8f6447dea4b39cf
Christian Scholz <cs@comlounge.net>
2010-08-19 22:23:27 +0200

    removed the subapp stuff and considerly simplified app handling. Also fixed tests which run

--- quantumlounge/framework/app.py
+++ quantumlounge/framework/app.py
@@ -6,67 +6,19 @@ import handler
 class Application(object):
     """a base class for dispatching WSGI requests"""
     
-    handlers = []
-    sub_apps = {} # mapping prefix => application
-    
     def __init__(self, settings={}, prefix=""):
         """initialize the Application with a settings dictionary and an optional
         ``prefix`` if this is a sub application"""
-        print "***",self,"***", prefix
         self.settings = settings
-        self.prefix = prefix
-        self.setup_subapps()
-        self.setup_mapper()
-    
-    def setup_subapps(self):
-        """set up the sub applications by instantiating them"""
-        prefix = self.prefix
-        self.sub_mapper = routes.Mapper() # for sub apps
-        self.sub_prefixes = {}
-        
-        for path,app in self.sub_apps.items():
-            # instantiate if not instantiated already
-            if path[0]!="/": path = "/"+path
-            if not isinstance(app, Application):
-                app = self.sub_apps[path] = app(self.settings, path)
-            # now add to sub mappers
-            path = prefix+path
-            if path[-1]=='/' and path!="/":
-                path = path[:-1]
-            print path
-            self.sub_mapper.connect(path, app=app)
-            self.sub_prefixes[path] = app
-        
-    def setup_mapper(self):
-        """setup the Routes mapper"""
-        prefix = self.prefix
         self.mapper = routes.Mapper()
-        for path, handler in self.handlers:
-        
-            path = prefix+path
-            if path[-1]=='/' and path!="/":
-                path = path[:-1]
-            self.mapper.connect(path, handler=handler)
-        
+        self.setup_handlers(self.mapper)
+
     def __call__(self, environ, start_response):
         request = werkzeug.Request(environ)
         path = environ['PATH_INFO']
         
         if len(path)>0 and path[-1]=="/" and path!='/':
             path = path[:-1]
-
-        print "called with %s" %path
-        # first check sub apps
-        m = self.sub_mapper.match(path)
-        if m is not None:
-            app = m['app']
-            return app(environ, start_response)
-        
-        for a,v in self.sub_prefixes.items():
-            print path, a,v
-            if path.startswith(a):
-                print "yes"
-                return v(environ, start_response)
 
         m = self.mapper.match(path)
         if m is not None:
--- quantumlounge/framework/tests/test_app_basics.py
+++ quantumlounge/framework/tests/test_app_basics.py
@@ -14,28 +14,12 @@ class TestHandler3(Handler):
     def get(self, id=''):
         return werkzeug.Response(str(id))
     
-class SubApp(Application):
-    """a sub application mounted to /subapp in App1"""
-    handlers = (
-        ("/", TestHandler1),
-        ("/foobar", TestHandler2),
-    )
-
-class SubApp2(SubApp):
-    """another one"""    
-
 class App1(Application):
-    
-    sub_apps = {
-        '/subapp' : SubApp,
-        '/subapp/another' : SubApp2}
-    
-    handlers = (
-        ("/", TestHandler1),
-        ("/huhu", TestHandler2),
-        ("/post/{id}", TestHandler3),
-        
-    )
+   
+    def setup_handlers(self, map):
+        map.connect(None, "/", handler=TestHandler1)
+        map.connect(None, "/huhu", handler=TestHandler2)
+        map.connect(None, "/post/{id}", handler=TestHandler3)
 
 def test_app_basics():
 
@@ -67,30 +51,6 @@ def test_app_unkown_path():
     resp = c.post('/no')
     assert resp.status=="404 NOT FOUND"
     
-def test_subhandler():
-
-    app = App1({'foo':'bar'}) # simple settings dict
-    c = werkzeug.Client(app, werkzeug.BaseResponse)
-
-    resp = c.get('/subapp')
-    assert resp.status=="200 OK"
-    
-def test_pathed_subhandler():
-
-    app = App1({'foo':'bar'}) # simple settings dict
-    c = werkzeug.Client(app, werkzeug.BaseResponse)
-
-    resp = c.get('/subapp/another')
-    assert resp.status=="200 OK"
-
-def test_pathed_subhandler_with_path():
-
-    app = App1({'foo':'bar'}) # simple settings dict
-    c = werkzeug.Client(app, werkzeug.BaseResponse)
-
-    resp = c.get('/subapp/another/foobar')
-    assert resp.status=="200 OK"
-
 def test_path_params():
 
     app = App1({'foo':'bar'}) # simple settings dict
