7c572ad68c06175551bf088d74da0feac3d886c0
Christian Scholz <mrtopf@gmail.com>
2010-08-22 20:02:16 +0200

    converted @json decorator to a class to be able to pass in headers to set

--- quantumlounge/framework/decorators.py
+++ quantumlounge/framework/decorators.py
@@ -18,15 +18,26 @@ def html(method):
     return wrapper
         
 
-def json(method):
-    """takes a dict output of a handler method and returns it as JSON"""
+class json(object):
     
-    @functools.wraps(method)
-    def wrapper(*args, **kwargs):
-        data = method(*args, **kwargs)
-        response = werkzeug.Response(simplejson.dumps(data))
-        response.content_type = "application/json"
-        return response
+    def __init__(self, **headers):
+        self.headers = {}
+        for a,v in headers.items():
+            ps = a.split("_")
+            ps = [p.capitalize() for p in ps]
+            self.headers["-".join(ps)] = v
+    
+    def __call__(self, method):
+        """takes a dict output of a handler method and returns it as JSON"""
+    
+        @functools.wraps(method)
+        def wrapper(*args, **kwargs):
+            data = method(*args, **kwargs)
+            response = werkzeug.Response(simplejson.dumps(data))
+            response.content_type = "application/json"
+            for a,v in self.headers.items():
+                response.headers[a] = v
+            return response
 
-    return wrapper
+        return wrapper
         
