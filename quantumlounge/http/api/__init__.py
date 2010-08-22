c243ee1599dff34a3c8d514ea78dcd6d01cd9af9
Christian Scholz <mrtopf@gmail.com>
2010-08-22 20:55:59 +0200

    added token endpoint and poco endpoint

--- quantumlounge/http/api/__init__.py
+++ quantumlounge/http/api/__init__.py
@@ -4,4 +4,6 @@ def setup_handlers(map):
     """setup the handlers"""
     with map.submapper(path_prefix="/api/1/users") as m:
         m.connect(None, '/login', handler=usermanager.Login)
+        m.connect(None, '/token', handler=usermanager.Token)
+        m.connect(None, '/u/{username}/profile', handler=usermanager.PoCo)
     
--- quantumlounge/http/api/usermanager.py
+++ quantumlounge/http/api/usermanager.py
@@ -1,6 +1,8 @@
-from quantumlounge.framework import Handler, Application
+from quantumlounge.framework import Handler, json, html
 import werkzeug
 import simplejson
+
+import quantumlounge.usermanager.errors as errors
 
 class Login(Handler):
     """handle logging in users. On a POST it will check username and password 
@@ -30,3 +32,67 @@ class Login(Handler):
         res.content_type = "application/json"
         return res
 
+class Token(Handler):
+    """Retrieve an access token for an authorization code
+    
+    TODO: Add description of view
+    """
+    
+    def error(self, code="invalid_request", error_message=''):
+        return {
+            'error' : code,
+            'error_message' : error_message
+        }
+
+    @json(cache_control = "no-store")
+    def get(self):
+        code = self.request.args.get("code", None)
+        client_id = self.request.args.get("client_id", None)
+        if code is None or client_id is None:
+            return self.error("invalid_request", "code or client_id missig from request")
+        
+        # try to obtain an access token for the auth code sent
+        am = self.app.settings['authmanager']
+        try:
+            token = am.get_token_for_code(code, client_id)
+        except errors.UserManagerError, e:
+            return self.error(e.code, e.msg)
+
+        return {
+            'access_token' : token.token,
+            'username' : token.username
+        }
+
+class PoCo(Handler):
+    """Return the Portable Contacts data for a user.
+    
+    We receive an access token in the request parameters which we will check for validity
+    
+    TODO: Add description of view
+    """
+    
+    def error(self, code="invalid_request", error_message=u''):
+        return {
+            'error' : code,
+            'error_message' : error_message
+        }
+    
+    @json()
+    def get(self, username):
+        am = self.app.settings['authmanager']
+        um = self.app.settings['usermanager']
+        
+        access_token = self.request.args.get("access_token", None)
+        if access_token is None:
+            return self.error(error_message="no access token was given")
+        token = am.get_token(access_token, None)
+        if token is None:
+            return self.error('invalid_grant', 'The authorization token is not valid')
+        if token.username != username:
+            return self.error('invalid_grant', 'The authorization token is not valid')
+        
+        # finally do somethign
+        u = um.get('mrtopf')
+        if u is None:
+            return self.error(error_message="no access token was given")
+        return u.get_poco()
