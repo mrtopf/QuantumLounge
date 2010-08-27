78db923aa11718f3f62fb9c9f4b7496b24c797eb
Christian Scholz <mrtopf@gmail.com>
2010-08-27 18:44:20 +0200

    fixed tests and test setup to run again with new URL structure

--- quantumlounge/http/usermanager/tests/conftest.py
+++ quantumlounge/http/usermanager/tests/conftest.py
@@ -1,11 +1,10 @@
-from quantumlounge.http.usermanager.usermanager import UserManager
+from quantumlounge.http.main import App
 from quantumlounge.http.setup import setup
 import werkzeug
 
-
 def pytest_funcarg__um_client(request):
     """return a client object for testing"""
-    um = UserManager(setup())    
+    um = App(setup())    
     return werkzeug.Client(um, werkzeug.BaseResponse)
     
     
--- quantumlounge/http/usermanager/tests/test_login_handler.py
+++ quantumlounge/http/usermanager/tests/test_login_handler.py
@@ -1,26 +1,26 @@
 import simplejson
 
 def test_login(um_client):
-    r = um_client.post("/api/login", data={'username': 'mrtopf', 'password' : 'foobar'})
+    r = um_client.post("/api/1/users/login", data={'username': 'mrtopf', 'password' : 'foobar'})
     assert r.headers['Content-Type'] == "application/json"
     assert r.status == "200 OK"
     d = simplejson.loads(r.data)
     assert d['username'] == "mrtopf"
 
 def test_login_with_wrong_pw(um_client):
-    r = um_client.post("/api/login", data={'username': 'mrtopf', 'password' : 'foobar2'})
+    r = um_client.post("/api/1/users/login", data={'username': 'mrtopf', 'password' : 'foobar2'})
     assert r.status == "401 UNAUTHORIZED"
 
 def test_login_with_wrong_user(um_client):
-    r = um_client.post("/api/login", data={'username': 'mrtopf222', 'password' : 'foobar'})
+    r = um_client.post("/api/1/users/login", data={'username': 'mrtopf222', 'password' : 'foobar'})
     assert r.status == "404 NOT FOUND"
 
 def test_login_with_missing_parameters(um_client):
-    r = um_client.post("/api/login", data={})
+    r = um_client.post("/api/1/users/login", data={})
     assert r.status == "400 BAD REQUEST"
-    r = um_client.post("/api/login", data={'username' : 'mrtopf'})
+    r = um_client.post("/api/1/users/login", data={'username' : 'mrtopf'})
     assert r.status == "400 BAD REQUEST"
-    r = um_client.post("/api/login", data={'password' : 'nopw'})
+    r = um_client.post("/api/1/users/login", data={'password' : 'nopw'})
     assert r.status == "400 BAD REQUEST"
     
     
