4251408d485f23c1cb6090da95c38e1eff409d5c
Christian Scholz <mrtopf@gmail.com>
2010-09-18 21:21:53 +0200

    adding custom scss file for styling PM

--- quantumlounge/http/pm/__init__.py
+++ quantumlounge/http/pm/__init__.py
@@ -1,18 +1,21 @@
+import pkg_resources
 import main
+from quantumlounge.framework import StaticHandlerFactory
 from quantumcore.resources import js_from_pkg_stream, css_from_pkg_stream
 
 def setup_handlers(map):
     """mapper setup for the project manager"""
     with map.submapper(path_prefix="/pm") as m:
+        m.connect(None, '/templates/{path_info:.*}', handler = StaticHandlerFactory(pkg_resources.resource_filename(__name__, 'templates')))
         m.connect(None, '', handler=main.Main)
 
 def setup_js():
     return [
-        js_from_pkg_stream(__name__, 'main.js', name="http.pm.main", merge=False, prio=5,),
+        js_from_pkg_stream(__name__, 'main.js', name="http.pm.main", merge=False, prio=5, auto_reload=True),
     ]
     
 def setup_css():
     return [
-        #css_from_pkg_stream(__name__, 'css/pm.css', name="http.pm.timeline", merge=False, prio=5,),
+        css_from_pkg_stream(__name__, 'pm.css', name="http.pm.main", merge=False, prio=5,auto_reload=True),
     ]
     
