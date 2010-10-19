97a539c0ffb04d060cb2903527903e448c8962e8
Christian Scholz <mrtopf@gmail.com>
2010-10-19 16:22:02 +0200

    fixed route handling to support all Routes features

--- quantumlounge/framework/app.py
+++ quantumlounge/framework/app.py
@@ -15,12 +15,7 @@ class Application(object):
 
     def __call__(self, environ, start_response):
         request = werkzeug.Request(environ)
-        path = environ['PATH_INFO']
-        
-        if len(path)>0 and path[-1]=="/" and path!='/':
-            path = path[:-1]
-
-        m = self.mapper.match(path)
+        m = self.mapper.match(environ = environ)
         if m is not None:
             handler = m['handler'](app=self, request=request, settings=self.settings)
             method = request.method.lower()
