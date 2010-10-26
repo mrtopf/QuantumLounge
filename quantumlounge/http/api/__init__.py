107c31e3474100fd9330089653609fa73e345866
Christian Scholz <mrtopf@gmail.com>
2010-10-26 12:39:25 +0200

    fixed /names API

--- quantumlounge/http/api/__init__.py
+++ quantumlounge/http/api/__init__.py
@@ -4,10 +4,12 @@ import users
 def setup_handlers(map):
     """setup the handlers"""
     with map.submapper(path_prefix="/api/1/users") as m:
-        m.connect(None, '/names', handler=usermanager.Login)
+        m.connect(None, '/names', handler=usermanager.Names)
         m.connect(None, '/login', handler=usermanager.Login)
         m.connect(None, '/token', handler=usermanager.Token)
         m.connect(None, '/u/{username}/profile', handler=usermanager.PoCo)
+
+
     with map.submapper(path_prefix="/api/1/tweets") as m:
         #m.connect(None, '/{path_info:.*}', handler = content.ContentHandler)
         # routes for now which use GET and POST on tweets
