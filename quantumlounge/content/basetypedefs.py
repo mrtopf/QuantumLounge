71183ee3dcc6016a4427e0db6844d6c9f933bc41
Christian Scholz <mrtopf@gmail.com>
2010-10-26 15:00:14 +0200

    removed old tweet type and replaced it with new file only registering the content type "status"

---
+++ quantumlounge/content/basetypedefs.py
@@ -0,0 +1,17 @@
+from base import Status, StatusCollection
+from contenttypes import ContentType
+
+def TweetType(db, coll):
+    tm = StatusCollection(db, coll)
+    return ContentType(
+        u"status",
+        name = u"Status",
+        description = "a status message",
+        fields = Status._attribs,
+        required_fields = ['content', 'user'],
+        mgr = tm,
+        cls = Status,
+        reprs = ['default', 'atom'],
+        default_repr = "default"
+    )
+
