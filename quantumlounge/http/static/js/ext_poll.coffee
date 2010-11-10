f645ce45f1bc1ca78b85a592094519212551fcc1
Christian Scholz <mrtopf@gmail.com>
2010-11-10 18:14:11 +0100

    finished first round of implementation for external poll script

--- quantumlounge/http/static/js/ext_poll.coffee
+++ quantumlounge/http/static/js/ext_poll.coffee
@@ -1,32 +1,85 @@
-
-
 class PollProcessor
     content_api: "/api/1/content/"
     template_api: "/api/templates/"
 
     constructor: (@baseurl) ->
         @template = ""
-        # load template
-        url = @baseurl+@template_api+'entry.poll.mustache'
+        @voted = false
+
+        # retrieve the most recent object and initialize the template then
+        $.ajax({
+            url: @baseurl+@content_api+"0?r=jsview&jsview_type=poll&so=date&sd=down&l=1"
+            dataType: "jsonp"
+            success: (data) =>
+                @item = data.jsview[0]
+                @item_id = @item._id
+                $.ajax({
+                    url : @baseurl+@content_api+@item._id+";voted"
+                    dataType: "jsonp"
+                    success: (data) =>
+                        @voted = data.voted
+                        @load_template()
+                })
+            })
+
+    load_template: () =>
+        if (@voted)
+            url = @baseurl+@template_api+'entry.poll.results.mustache'
+        else
+            url = @baseurl+@template_api+'entry.poll.mustache'
         $.ajax {
             url: url
             dataType: 'jsonp'
             success: (data) =>
                 @template = data
+                if (@voted)
+                    @display_results()
+                else
+                    @display_poll()
         }
-    display_items: ->
+
+    display_poll: () ->
+        # convert answers
+        new_answers = []
+        i=0
+        for answer in @item.answers
+            new_answers.push({
+                title: answer
+                no: i
+            })
+            i=i+1
+        @item.answers = new_answers
+        h = $(Mustache.to_html(@template, @item))
+        $("#poll").html(h)
+        $(".ql-poll-box").change(@vote)
+
+    display_results: ->
         $.ajax({
-            url: "http://localhost:9991/api/1/content/0?r=jsview&jsview_type=poll&so=date&sd=down&l=1"
+            url : @baseurl+@content_api+@item._id+";results"
             dataType: "jsonp"
             success: (data) =>
-                console.log data
-                for key, item of data.jsview
-                    $(Mustache.to_html(@template, item)).appendTo("#poll")
+                h = $(Mustache.to_html(@template, data))
+                $("#poll").html(h)
+        })
+
+    vote: (ev) =>
+        elem = ev.target
+        qid = $(elem).attr('data-qid')
+        aid = $(elem).attr('data-aid')
+        url = @baseurl+@content_api+qid+";vote"
+        data = {answer_no: aid}
+        $.ajax({
+            url: url
+            dataType: 'jsonp'
+            type: "GET"
+            data: data 
+            success: (data) =>
+                @voted = true
+                @load_template()
         })
 
 
 $(document).ready(
   ->
       p = new PollProcessor "http://localhost:9991"
-      p.display_items()
 )
--- quantumlounge/http/static/js/ext_poll.js
+++ quantumlounge/http/static/js/ext_poll.js
@@ -2,43 +2,102 @@
   var PollProcessor;
   var __bind = function(func, context) {
     return function(){ return func.apply(context, arguments); };
-  }, __hasProp = Object.prototype.hasOwnProperty;
-  PollProcessor = function(_a) {
-    var url;
-    this.baseurl = _a;
+  };
+  PollProcessor = function(_b) {
+    var _a;
+    this.baseurl = _b;
+    _a = this;
+    this.vote = function(){ return PollProcessor.prototype.vote.apply(_a, arguments); };
+    this.load_template = function(){ return PollProcessor.prototype.load_template.apply(_a, arguments); };
     this.template = "";
-    url = this.baseurl + this.template_api + 'entry.poll.mustache';
+    this.voted = false;
     $.ajax({
-      url: url,
-      dataType: 'jsonp',
+      url: this.baseurl + this.content_api + "0?r=jsview&jsview_type=poll&so=date&sd=down&l=1",
+      dataType: "jsonp",
       success: __bind(function(data) {
-        return (this.template = data);
+        this.item = data.jsview[0];
+        this.item_id = this.item._id;
+        return $.ajax({
+          url: this.baseurl + this.content_api + this.item._id + ";voted",
+          dataType: "jsonp",
+          success: __bind(function(data) {
+            this.voted = data.voted;
+            return this.load_template();
+          }, this)
+        });
       }, this)
     });
     return this;
   };
   PollProcessor.prototype.content_api = "/api/1/content/";
   PollProcessor.prototype.template_api = "/api/templates/";
-  PollProcessor.prototype.display_items = function() {
+  PollProcessor.prototype.load_template = function() {
+    var url;
+    if (this.voted) {
+      url = this.baseurl + this.template_api + 'entry.poll.results.mustache';
+    } else {
+      url = this.baseurl + this.template_api + 'entry.poll.mustache';
+    }
     return $.ajax({
-      url: "http://localhost:9991/api/1/content/0?r=jsview&jsview_type=poll&so=date&sd=down&l=1",
+      url: url,
+      dataType: 'jsonp',
+      success: __bind(function(data) {
+        this.template = data;
+        return (this.voted) ? this.display_results() : this.display_poll();
+      }, this)
+    });
+  };
+  PollProcessor.prototype.display_poll = function() {
+    var _a, _b, _c, answer, h, i, new_answers;
+    new_answers = [];
+    i = 0;
+    _b = this.item.answers;
+    for (_a = 0, _c = _b.length; _a < _c; _a++) {
+      answer = _b[_a];
+      new_answers.push({
+        title: answer,
+        no: i
+      });
+      i = i + 1;
+    }
+    this.item.answers = new_answers;
+    h = $(Mustache.to_html(this.template, this.item));
+    $("#poll").html(h);
+    return $(".ql-poll-box").change(this.vote);
+  };
+  PollProcessor.prototype.display_results = function() {
+    return $.ajax({
+      url: this.baseurl + this.content_api + this.item._id + ";results",
       dataType: "jsonp",
       success: __bind(function(data) {
-        var _a, _b, item, key;
-        console.log(data);
-        _a = []; _b = data.jsview;
-        for (key in _b) {
-          if (!__hasProp.call(_b, key)) continue;
-          item = _b[key];
-          _a.push($(Mustache.to_html(this.template, item)).appendTo("#poll"));
-        }
-        return _a;
+        var h;
+        h = $(Mustache.to_html(this.template, data));
+        return $("#poll").html(h);
+      }, this)
+    });
+  };
+  PollProcessor.prototype.vote = function(ev) {
+    var aid, data, elem, qid, url;
+    elem = ev.target;
+    qid = $(elem).attr('data-qid');
+    aid = $(elem).attr('data-aid');
+    url = this.baseurl + this.content_api + qid + ";vote";
+    data = {
+      answer_no: aid
+    };
+    return $.ajax({
+      url: url,
+      dataType: 'jsonp',
+      type: "GET",
+      data: data,
+      success: __bind(function(data) {
+        this.voted = true;
+        return this.load_template();
       }, this)
     });
   };
   $(document).ready(function() {
     var p;
-    p = new PollProcessor("http://localhost:9991");
-    return p.display_items();
+    return (p = new PollProcessor("http://localhost:9991"));
   });
 })();
